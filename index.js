d83b43c514c236b1ba043fe34b175eb53d5d2580
Ashley Cai <ashley.lcai@gmail.com>
2023-02-18 17:22:17 -0500

    redaction working

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 "use strict";
 let sentences = ["When are you free to give me a haircut? Please let me know, and remember to bring the hairdressing tools!"];
-let exceptionWords = ["a", "when", "you", "to", "please", "me", "and", "the", " ", ".", "?", "!",];
-//Randomly picks a sentence from the list of sentences and 
+let exceptionWords = ["a", "when", "you", "to", "please", "me", "and", "the", " ", ".", "?", "!", ","];
+let words;
+let lowercaseWords;
+let redactedWords;
+let sentenceObject;
+//Randomly picks a sentence from the list of sentences and initializes/displays it
 function initializeSentence() {
+    //temporary
     let str = sentences[0];
-    let words = str.match(/\w+|\s+|[^\s\w]+/g);
-    let redactedWords = redactSentence(words);
-    let sentenceObject = document.getElementById("sentence");
+    //split sentence into separate pieces and store in 2 lists - nonredacted and redacted
+    words = str.match(/\w+|\s+|[^\s\w]+/g);
+    lowercaseWords = stringArrayLowercase(words);
+    redactedWords = redactSentence(words);
+    //get printable version of redacted sentence and display
+    sentenceObject = document.getElementById("sentence");
     sentenceObject.innerHTML = sentenceFromList(redactedWords);
     console.log(words);
 }
@@ -18,7 +26,15 @@ function sentenceFromList(arr) {
     }
     return str;
 }
-//returns sentence with redacted words
+//takes in array of strings and returns the same in lowercase
+function stringArrayLowercase(arr) {
+    let lowercaseArr = [];
+    for (let i = 0; i < arr.length; ++i) {
+        lowercaseArr.push(arr[i].toLowerCase());
+    }
+    return lowercaseArr;
+}
+//returns sentence with redacted words -- used by initialize sentence
 function redactSentence(words) {
     let redactedWords = [];
     for (let i = 0; i < words.length; ++i) {
@@ -31,13 +47,25 @@ function redactSentence(words) {
     }
     return redactedWords;
 }
-//returns redacted string based on given length
+//returns redacted string based on given length -- used by redactSentence
 function redactedGenerator(length) {
     let str = '';
     for (let i = 0; i < length; ++i) {
         str += '█';
     }
     return str;
+}
+//Handles guesses from the guess input text box, unredacts if there is a match
+function guess() {
+    var _a;
+    let formObject = document.getElementById("guess");
+    let input = (_a = document.getElementById("guess")) === null || _a === void 0 ? void 0 : _a.value;
+    formObject.value = "";
+    let index = lowercaseWords.indexOf(input.toLowerCase());
+    if (index != -1) {
+        redactedWords[index] = words[index];
+        sentenceObject.innerHTML = sentenceFromList(redactedWords);
+    }
 }
 window.onload = function () {
     initializeSentence();
--- index.ts
+++ index.ts
@@ -2,14 +2,25 @@
 
 let sentences: string[] = ["When are you free to give me a haircut? Please let me know, and remember to bring the hairdressing tools!"]
 
-let exceptionWords: string[] = ["a","when","you","to","please","me","and","the"," ",".","?","!",]
+let exceptionWords: string[] = ["a","when","you","to","please","me","and","the"," ",".","?","!",","]
 
-//Randomly picks a sentence from the list of sentences and 
+let words: string[];
+let lowercaseWords: string[];
+let redactedWords: string[];
+let sentenceObject: HTMLElement;
+
+//Randomly picks a sentence from the list of sentences and initializes/displays it
 function initializeSentence(){
+    //temporary
     let str = sentences[0]
-    let words = str.match(/\w+|\s+|[^\s\w]+/g)
-    let redactedWords = redactSentence(words!)
-    let sentenceObject = document.getElementById("sentence")
+
+    //split sentence into separate pieces and store in 2 lists - nonredacted and redacted
+    words = str.match(/\w+|\s+|[^\s\w]+/g)!
+    lowercaseWords = stringArrayLowercase(words!)
+    redactedWords = redactSentence(words!)
+
+    //get printable version of redacted sentence and display
+    sentenceObject = document.getElementById("sentence")!
     sentenceObject!.innerHTML = sentenceFromList(redactedWords);
     console.log(words)
 }
@@ -18,12 +29,21 @@ function initializeSentence(){
 function sentenceFromList(arr: string[]){
     let str = '';
     for (let i = 0; i < arr.length; ++i) {
-        str += arr[i];
+        str += arr[i]
     }
-    return str;
+    return str
 }
 
-//returns sentence with redacted words
+//takes in array of strings and returns the same in lowercase
+function stringArrayLowercase(arr: string[]) {
+    let lowercaseArr: string[] = [];
+    for (let i = 0; i < arr.length; ++i) {
+        lowercaseArr.push(arr[i].toLowerCase())
+    }
+    return lowercaseArr
+}
+
+//returns sentence with redacted words -- used by initialize sentence
 function redactSentence(words: string[]) {
     let redactedWords: string[] = []
     for (let i = 0; i < words.length; ++i) {
@@ -37,7 +57,7 @@ function redactSentence(words: string[]) {
     return redactedWords
 }
 
-//returns redacted string based on given length
+//returns redacted string based on given length -- used by redactSentence
 function redactedGenerator(length: number) {
     let str = '';
     for (let i = 0; i < length; ++i) {
@@ -46,6 +66,18 @@ function redactedGenerator(length: number) {
     return str;
 }
 
+//Handles guesses from the guess input text box, unredacts if there is a match
+function guess() {
+    let formObject = document.getElementById("guess")
+    let input = document.getElementById("guess")?.value
+    formObject!.value = ""
+
+    let index = lowercaseWords.indexOf(input.toLowerCase())
+    if (index != -1) {
+        redactedWords[index] = words[index]
+        sentenceObject!.innerHTML = sentenceFromList(redactedWords);
+    }
+}
 
 window.onload = function() {
     initializeSentence();

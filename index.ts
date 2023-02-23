0f5e479c72411aeb2b34530179540b87d69b15d4
Ashley Cai <ashley.lcai@gmail.com>
2023-02-22 22:02:33 -0500

    test

--- index.ts
+++ index.ts
@@ -171,10 +171,3 @@ function closeAboutModal() {
 window.onload = function() {
     initializeSentence();
   };
-
-  var el = document.getElementById("editable");
-  var scrollTop = document.body.scrollTop; // save position
-  el.focus();
-  // manipulate selection inside the focused element with `document.createRange()`
-  // and do other stuff
-  document.body.scrollTop = scrollTop;
